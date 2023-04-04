d2546328c1e175dd094f5faf7fbbd084b10aacd1
Aaquib Asrar <maquib22@gmail.com>
2023-04-04 21:56:29 +0530

    random number generator

---
+++ 55-random-number-generator/script.js
@@ -0,0 +1,12 @@
+
+const number = document.querySelector('.number')
+const btn = document.querySelector('.generate')
+const generateNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 10 + 1)
+    number.innerHTML = randomNumber;
+}
+btn.addEventListener('click', generateNumber);
+generateNumber()
+
+// const rand = Math.floor(Math.random() * 10 +1);
+// console.log(rand);

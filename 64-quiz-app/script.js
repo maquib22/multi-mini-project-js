181ee11de4cefdf509c8d62753cf55b5a64dc5f4
Aaquib Asrar <maquib22@gmail.com>
2023-04-19 23:17:18 +0530

    quiz app

--- 64-quiz-app/script.js
+++ 64-quiz-app/script.js
@@ -5,11 +5,11 @@ class Quiz {
         this.questionIndex = 0;
     }
 
-    getQuestionsIndex(){
+    getQuestionIndex(){
         return this.questions[this.questionIndex]
     }
     guess(answer) {
-        if(this.getQuestionsIndex().isCorrectAnswer(answer)){
+        if(this.getQuestionIndex().isCorrectAnswer(answer)){
             this.score++;
         }
         this.questionIndex++;
@@ -22,30 +22,30 @@ class Quiz {
 
 // ======== Question class ===========
 
-class Questions {
+class Question   {
     constructor(text, choices, answer){
         this.text = text;
         this.choices = choices;
         this.answer = answer
     }
 
-    isCorrectAnswer(choices) {
-        return this.answer === choices
+    isCorrectAnswer(choice) {
+        return this.answer === choice
     }
 }
 
 // ======== display question ====
 
-function displayQuestions(){
+function displayQuestion(){
     if(quiz.isEnded){
         showScores()
     }else {
         // show question
         let questionElement = document.getElementById("question");
-        questionElement.innerHTML = quiz.getQuestionsIndex().text;
+        questionElement.innerHTML = quiz.getQuestionIndex().text;
 
         //=========show option ==========
-        let choices = quiz.getQuestionsIndex().choices;
+        let choices = quiz.getQuestionIndex().choices;
         for(let i = 0; i < choices.length; i++) {
             let choiceElement = document.getElementById("choice" + i);
             choiceElement.innerHTML = choices[i]
@@ -61,7 +61,7 @@ function guess(id, guess) {
     let button = document.getElementById(id);
     button.onclick = function(){
         quiz.guess(guess);
-        displayQuestions()
+        displayQuestion()
     }
 }
 
@@ -75,7 +75,8 @@ function showProgress(){
 
 // show score 
 function showScores() {
-    let quizEndHtml = `
+    let quizEndHtml = 
+    `
     <h1> Quiz Completed</h1>
     <h2 id="score"> You Scored: ${quiz.score} of ${quiz.questions.length}<h2>
     <div class="quiz-repeat">
@@ -89,20 +90,44 @@ function showScores() {
 
 //======= creat quiz question
 
-let question = [
-    new Questions(
+let questions = [
+    new Question(
         "Hyper Text Markup Language Stand for?", ["jQuary", "XHTML", "CSS", "HTML"], "HTML"
     ),
-    new Questions(
+    new Question(
         "Which is a JavaScript Framework?", ["React", "Laravel", "Python", "Scss"], "React"
     ),
-    new Questions(
+    new Question(
         "Which is a Backend Language", ["PHP", "HTML", "Reaxt", "All"], "PHP"
     ),
 ];
 
-let quiz = new Quiz(question);
+let quiz = new Quiz(questions);
 
 // display quistion
 
-displayQuestions();
+displayQuestion();
+
+//=========== Add timer ======
+let time = 1;
+let quizTimeInMinutes = time * 60 *60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+    let quizTimer = setInterval(function() {
+        if(quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores()
+        }else{
+            quizTime--;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60) % 60;
+            counting.innerHTML = `TIME: ${min} : ${sec}`
+
+        }
+    },1000);
+}
+
+startCountdown()
